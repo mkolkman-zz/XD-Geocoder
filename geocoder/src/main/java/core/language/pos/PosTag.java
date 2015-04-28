5f514427d391336e56ffd21ccdff991692f75085
Mike Kolkman [Label305] <mike@label305.com>
2015-04-28 10:56:15 +0200

    added pos tag class

---
+++ geocoder/src/main/java/core/language/pos/PosTag.java
@@ -0,0 +1,7 @@
+package core.language.pos;
+
+import java.io.Serializable;
+
+public enum PosTag implements Serializable {
+    NOUN, PRONOUN, ADJECTIVE, ADVERB, CONJUNCTION, INTERJECTION, PREPOSITION, VERB, ARTICLE, INTERPUNCTION, DETERMINER, UNKNOWN, OTHER
+}
